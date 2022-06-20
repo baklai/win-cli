62409bed5f45abfbd87b38d18e57ea5e7e25dc84
Dmitrii <d.n.baklai@gmail.com>
2022-06-20 14:13:27 +0300

    add pwa

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,67 @@ export default theme({
   telemetry: false,
   ssr: false,
 
+  cli: {
+    badgeMessages: [
+      `Application: ${process.env.npm_package_name.toUpperCase()}`,
+      `Version:     ${process.env.npm_package_version}`
+    ],
+    bannerColor: 'green'
+  },
+
+  head: {
+    titleTemplate: '%s • Windows command line interpreter',
+    title: 'CLI Windows',
+    meta: [
+      { charset: 'utf-8' },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1, user-scalable=no'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Windows command line interpreter'
+      },
+      { name: 'google', content: 'notranslate' }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+
+  buildModules: ['@nuxtjs/pwa'],
+
+  pwa: {
+    meta: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      mobileApp: true,
+      mobileAppIOS: true,
+      appleStatusBarStyle: 'black',
+      favicon: true,
+      name: 'CLI Windows',
+      author: 'Dmitrii Baklai',
+      description: 'Windows command line interpreter',
+      theme_color: '#000',
+      lang: 'ru',
+      ogType: 'website',
+      ogHost: 'win-cli.netlify.app',
+      ogImage: {
+        path: '/icon.png',
+        width: '50',
+        height: '50',
+        type: 'image/png'
+      }
+    },
+    manifest: {
+      name: 'CLI Windows',
+      short_name: 'CLI Windows',
+      description: 'Windows command line interpreter',
+      lang: 'ru',
+      display: 'standalone',
+      background_color: '#000'
+    }
+  },
+
   docs: {
     primaryColor: '#00A2E9'
   },
