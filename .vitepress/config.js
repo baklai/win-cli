0cacd3a2baf2a3a557b20229fcbd51f60eab2af9
Dmitrii <d.n.baklai@gmail.com>
2023-08-02 22:35:42 +0300

    fix: out dir

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitepress';
 export default defineConfig({
   base: '/',
   srcDir: 'src',
-  outDir: '../dist',
+  outDir: 'dist',
 
   appearance: true,
   cleanUrls: true,
