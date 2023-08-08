086fbb73e6295fd025f86013046c79a88a304d72
Dmitrii <d.n.baklai@gmail.com>
2023-08-08 16:00:15 +0300

    fix: comment sitemap object in config

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -140,10 +140,10 @@ export default withPwa(
     cleanUrls: true,
     lastUpdated: true,
 
-    sitemap: {
-      hostname: HOST_NAME,
-      lastmodDateOnly: false
-    },
+    // sitemap: {
+    //   hostname: HOST_NAME,
+    //   lastmodDateOnly: false
+    // },
 
     lang: 'uk-UA',
     title: 'WIN:\\CLI',
