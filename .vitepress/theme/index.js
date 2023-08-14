3044e99f5b5018e1ef3be23518b179dd379c8661
Dmitrii <d.n.baklai@gmail.com>
2023-08-14 09:58:59 +0300

    fix: edit theme config

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,11 +1,4 @@
-import { h } from 'vue';
-import Theme from 'vitepress/theme';
+import DefaultTheme from 'vitepress/theme';
 import './style.css';
 
-export default {
-  extends: Theme,
-  Layout: () => {
-    return h(Theme.Layout, null, {});
-  },
-  enhanceApp({ app, router, siteData }) {}
-};
+export default DefaultTheme;
